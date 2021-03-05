fbe91eb12e0ccae2f4cefab7a4891fab404fd318
Buğra <bugra.aras@protonmail.com>
2021-03-05 21:37:34 +0300

    Implemented game logic

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
-# Set up screen
+# Set up the screen
 screen = Screen()
 screen.setup(width=450, height=450)
 screen.screensize(350, 350)
@@ -14,15 +16,33 @@ screen.title('Snake')
 
 # Initialize the game
 snake = Snake()
-screen.listen()
+food = Food()
+scoreboard = Scoreboard()
 
+
+# Set up keys and start listening
+screen.listen()
+screen.onkey(snake.right, 'Right')
+screen.onkey(snake.right, 'd')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.left, 'a')
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.up, 'w')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.down, 's')
 
 # Start the game
 game_over = False
 while not game_over:
     screen.update()
-    time.sleep(0.1)
-    snake.move('R')
-    
+    time.sleep(0.07)
+    if snake.move():
+        if snake.head.distance(food) < 10:
+            scoreboard.increase()
+            snake.grow()
+            food.random()
+    else:
+        game_over = True
+        scoreboard.game_over()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -4,12 +4,15 @@ class Snake:
 
 
     def __init__(self):
+        ''' Constructs a three segment snake '''
         self._body = []
         for i in range(3):
-            self.extendBody(-i*10, 0)
+            self._extendBody(-i*10, 0)
+        self._direction = 'R'
+        self.head = self._body[0]
 
-    def extendBody(self, x, y):
-        ''' Extends the body from the tail by one segment '''
+    def _extendBody(self, x, y):
+        ''' Creates the body part for extension '''
         segment = Turtle('square')
         segment.shapesize(0.5, 0.5)
         segment.color(61, 80, 61)
@@ -17,26 +20,53 @@ class Snake:
         segment.goto(x, y)
         self._body.append(segment)
 
-    def _pos_to_move(self, direction):
+    def _pos_to_move(self):
         ''' Calculates the coordinates to move according to the direction and checks boundaries '''
-        current_x = self._body[0].xcor()
-        current_y = self._body[0].ycor()
+        current_x = self.head.xcor()
+        current_y = self.head.ycor()
         x_dirs = {'R': 10, 'L': -10}
         y_dirs = {'U': 10, 'D': -10}
-        current_x += x_dirs.get(direction, 0)
-        current_y += y_dirs.get(direction, 0)
-        boundaries = {180: -170, -180: 170}
-        current_x = boundaries.get(current_x, current_x)
-        current_y = boundaries.get(current_y, current_y)
+        current_x += x_dirs.get(self._direction, 0)
+        current_y += y_dirs.get(self._direction, 0)
+        x_boundaries = {180: -170, -180: 170}
+        y_boundaries = {140: -170, -180: 130}
+        current_x = x_boundaries.get(current_x, current_x)
+        current_y = y_boundaries.get(current_y, current_y)
         return (current_x, current_y)
 
-    def move(self, direction):
+    def move(self):
         ''' Moves the head towards the direction, and all the rest into the position of the segment before them '''
-        pos = self._pos_to_move(direction)
-        if pos != self._body[1].pos():
+        pos = self._pos_to_move()
+        if pos not in [s.pos() for s in self._body]:
             for i in range(len(self._body)-1, 0, -1):
                 new_x = self._body[i - 1].xcor()
                 new_y = self._body[i - 1].ycor()
                 self._body[i].goto(new_x, new_y)
-            self._body[0].goto(pos[0], pos[1])
-            
+            self.head.goto(pos[0], pos[1])
+            return True
+        
+        return False
+    
+    def right(self):
+        ''' Sets the direction of the snake to the right '''
+        if not self._direction == 'L':
+            self._direction = 'R'
+
+    def left(self):
+        ''' Sets the direction of the snake to the left '''
+        if not self._direction == 'R':
+            self._direction = 'L'
+
+    def up(self):
+        ''' Sets the direction of the snake upwards '''
+        if not self._direction == 'D':
+            self._direction = 'U'
+
+    def down(self):
+        ''' Sets the direction of the snake downwards '''
+        if not self._direction == 'U':
+            self._direction = 'D'
+
+    def grow(self):
+        ''' Extends the snake's body by one segment '''
+        self._extendBody(*self._body[-1].pos())
